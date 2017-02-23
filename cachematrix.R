f79f2a7797057a256428eb023a03c75009f65a6f
gallowar <richard_galloway@merck.com>
2017-02-23 15:35:51 -0500

    Updated Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions implement lexical scoping to determine the
+## inverse of a matrix that is passed as an argument.  There is
+## no validation checking so as per the assignment we are passing
+## a mtrix that has an inverse.  The inverse is computed only once
+## when the set function is called.  When the get function is called
+## the variable set is checked, if it is null the inverse is calculated
+## and stored.  If the variable is not null then the associated value
+## is returned from the previous execution.  The inverse is only
+## calcualted once and then referenced after that.
 
-## Write a short comment describing this function
-##
+## This routine accepts a matrix and defines the set
+## and get functions used for lexical scoping in 
+## finding the inverse of the matrix.
 makeCacheMatrix <- function(x = matrix()) {
 
   m <- NULL               # Initially set m to null   
@@ -25,8 +33,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-##
+## This function checks the variable defined in the set function.   
+## If that variable is null the inverse is calculated and the variable is set.
+## if the variable is null it returns the value from the inital set function call that 
+## that calculated the inverse and stored the result.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     m <- x$getinv()           # Set value
